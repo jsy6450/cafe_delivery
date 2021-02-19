7d9d350c64dc299f43c518c99ceac6166a89c938
jsy6450 <31643538+jsy6450@users.noreply.github.com>
2021-02-19 11:39:08 +0900

    Update PolicyHandler.java

--- delivery/src/main/java/cafeteria/PolicyHandler.java
+++ delivery/src/main/java/cafeteria/PolicyHandler.java
@@ -15,10 +15,18 @@ public class PolicyHandler{
 
     }
 
+    @Autowired
+    DeliveryRepository DeliveryRepository;
+    
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverMade_(@Payload Made made){
 
         if(made.isMe()){
+            Delivery delivery = new Delivery();
+            delivery.setOrderId(made.getId());
+            delivery.setStatus("Delivery Started");
+                         
+            DeliveryRepository.save(delivery);
             System.out.println("##### listener  : " + made.toJson());
         }
     }
